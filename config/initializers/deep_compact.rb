745e04b5872d691dd9df8b3292390be444b55b74
Mark Knutsen <mark.knutsen@cru.org>
2016-04-25 20:19:04 -0400

    Apply deep_compact! wherever GR hashes are constructed

---
+++ config/initializers/deep_compact.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+class Hash
+  def deep_compact!
+    reject! do |_, v|
+      if v.respond_to?('deep_compact!')
+        v.deep_compact!
+        v.empty?
+      else
+        v.nil?
+      end
+    end
+  end
+end
+
+class Array
+  def deep_compact!
+    reject! do |v|
+      if v.respond_to?('deep_compact!')
+        v.deep_compact!
+        v.empty?
+      else
+        v.nil?
+      end
+    end
+  end
+end
