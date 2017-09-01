d27a74f9d530e838f4d7ab86870d144f18c1919b
Nate Emerson <emerson.nate@gmail.com>
2017-09-01 09:38:59 -0400

    Refactor user_roles controller

--- app/controllers/v1/user_roles_controller.rb
+++ app/controllers/v1/user_roles_controller.rb
@@ -4,41 +4,43 @@ module V1
     power :superadmin
 
     def create
-      @ministry = Ministry.find_by(min_code: params[:ministry])
-      render_not_found and return if @ministry.nil?
-      add_admin
+      render_error('Invalid Ministry Code') and return unless load_ministry
+      if add_admin
+        render_admin
+      else
+        render_error('Adding admin failed', status: :internal_server_error)
+      end
     end
 
     def destroy
-      @ministry = Ministry.find_by(min_code: params[:ministry])
-      render_not_found and return if @ministry.nil?
-      remove_admin
+      render_error('Invalid Ministry Code') and return unless load_ministry
+      if remove_admin
+        render_admin
+      else
+        render_error('Admin not found', status: :not_found)
+      end
     end
 
     private
 
+    def load_ministry
+      @ministry ||= Ministry.find_by(min_code: min_code)
+    end
+
+    def min_code
+      params[:ministry] || params[:min_code]
+    end
+
     def add_admin
-      admin = @ministry.add_admin(params[:admin])
-      render status: 200, json: { success: add_success_message(admin) }
+      @admin = @ministry.add_admin(params[:admin])
     end
 
     def remove_admin
-      admin = @ministry.remove_admin(params[:admin])
-      if admin.nil?
-        render status: 404, json: { fail: remove_success_message(admin) }
-      else
-        render status: 200, json: { success: remove_success_message(admin) }
-      end
+      @admin = @ministry.remove_admin(params[:admin])
     end
 
-    def add_success_message(admin)
-      action_message = admin.nil? ? 'Admin already exists for' : 'added as admin to'
-      "#{admin&.person&.first_name} #{admin&.person&.last_name} #{action_message} #{@ministry.min_code}".strip
-    end
-
-    def remove_success_message(admin)
-      action_message = admin.nil? ? 'Admin not found for' : 'removed from'
-      "#{admin&.person&.first_name} #{admin&.person&.last_name} #{action_message} #{@ministry.min_code}".strip
+    def render_admin
+      render json: @admin.person, status: :ok, serializer: V1::BasicProfileSerializer unless @admin.blank?
     end
   end
 end
--- spec/requests/v1/user_roles_spec.rb
+++ spec/requests/v1/user_roles_spec.rb
@@ -42,30 +42,31 @@ RSpec.describe 'V1::User_Roles', type: :request do
 
       it 'adds an admin role' do
         create(:user_role, key_guid: user_key_guid, ministry: ministry_gr_id, role: UserRole.roles[:superadmin])
-        ministry = instance_double('Ministry', min_code: 'GUE')
-        role = instance_double('UserRole')
-        allow(role).to receive_message_chain(:person, :first_name).and_return('John')
-        allow(role).to receive_message_chain(:person, :last_name).and_return('Doe')
-        allow(ministry).to receive(:add_admin) { role }
-        allow(Ministry).to receive(:find_by) { ministry }
+        ministry = create(:ministry, min_code: 'GUE', gr_id: SecureRandom.uuid)
+        admin_key_guid = SecureRandom.uuid
+        admin_gr_id = SecureRandom.uuid
+        create(:person, first_name: 'John', last_name: 'Doe', key_guid: admin_key_guid, gr_id: admin_gr_id)
+        admin_roles = UserRole.where(ministry: ministry.gr_id).count
+        expect(admin_roles).to eq 0
 
         post '/v1/user_roles',
-             { admin: 'John.Doe@cru.org', ministry: 'GUE' },
+             { admin: admin_key_guid, ministry: 'GUE' },
              'HTTP_AUTHORIZATION' => "Bearer #{authenticate}"
 
+        admin_roles = UserRole.where(ministry: ministry.gr_id).count
+        expect(admin_roles).to eq 1
         expect(response).to be_success
         expect(response).to have_http_status :ok
-        expect(ministry).to have_received(:add_admin)
       end
 
-      it 'returns 404 for invalid ministry' do
+      it 'returns 400 for invalid ministry' do
         create(:user_role, key_guid: user_key_guid, ministry: ministry_gr_id, role: UserRole.roles[:superadmin])
         post '/v1/user_roles/',
              { admin: 'John.Doe@cru.org', ministry: 'DNE' },
              'HTTP_AUTHORIZATION' => "Bearer #{authenticate}"
 
         expect(response).not_to be_success
-        expect(response).to have_http_status 404
+        expect(response).to have_http_status :bad_request
       end
     end
   end
@@ -98,30 +99,30 @@ RSpec.describe 'V1::User_Roles', type: :request do
 
       it 'removes an admin role' do
         create(:user_role, key_guid: user_key_guid, ministry: ministry_gr_id, role: UserRole.roles[:superadmin])
-        ministry = instance_double('Ministry', min_code: 'GUE')
-        role = instance_double('UserRole')
-        allow(role).to receive_message_chain(:person, :first_name).and_return('John')
-        allow(role).to receive_message_chain(:person, :last_name).and_return('Doe')
-        allow(ministry).to receive(:remove_admin) { role }
-        allow(Ministry).to receive(:find_by) { ministry }
+        ministry = create(:ministry, min_code: 'GUE', gr_id: SecureRandom.uuid)
+        admin_key_guid = SecureRandom.uuid
+        admin_gr_id = SecureRandom.uuid
+        admin = create(:person, first_name: 'John', last_name: 'Doe', key_guid: admin_key_guid, gr_id: admin_gr_id)
+        ministry.add_admin(admin.key_guid)
 
         delete '/v1/user_roles',
-               { admin: 'John.Doe@cru.org', ministry: 'GUE' },
+               { admin: admin.key_guid, ministry: 'GUE' },
                'HTTP_AUTHORIZATION' => "Bearer #{authenticate}"
 
+        admin_roles = UserRole.where(ministry: ministry.gr_id).count
         expect(response).to be_success
         expect(response).to have_http_status :ok
-        expect(ministry).to have_received(:remove_admin)
+        expect(admin_roles).to eq 0
       end
 
-      it 'returns 404 for invalid ministry' do
+      it 'returns 400 for invalid ministry' do
         create(:user_role, key_guid: user_key_guid, ministry: ministry_gr_id, role: UserRole.roles[:superadmin])
         delete '/v1/user_roles/',
                { admin: 'John.Doe@cru.org', ministry: 'DNE' },
                'HTTP_AUTHORIZATION' => "Bearer #{authenticate}"
 
         expect(response).not_to be_success
-        expect(response).to have_http_status 404
+        expect(response).to have_http_status :bad_request
       end
 
       it 'returns 404 for invalid admin' do
